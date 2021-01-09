70b5d30e15827c9121c69f779ffd850621b25642
shaileh Bisht <shaileshbisht765@gmail.com>
2021-01-09 22:13:42 +0530

    add link in footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="footer_link">
-        <a href="">Home</a>
-        <p>\</p>
+        <Link to="/Home">Home</Link>
+        <p>|</p>
+        <Link to="/TermAndCondition">Terms and Conditions</Link>
+        <p>|</p>
+        <Link to="/Policy">Privacy Policy</Link>
+        <p>|</p>
+        <Link to="/ColStatement">Collection Statement</Link>
+        <p>|</p>
+        <Link to="/help">Help</Link>
       </div>
     </div>
   );

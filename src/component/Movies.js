3c672eed3ceb36ac729fa5836bab07b8f9fd423b
shaileh Bisht <shaileshbisht765@gmail.com>
2021-01-09 21:28:17 +0530

    add popular title to Movie Page

--- src/component/Movies.js
+++ src/component/Movies.js
@@ -1,10 +1,13 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
 import "./Movies.css";
 
 function Movies() {
   return (
     <div className="movies">
-      <h1>i am movies</h1>
+      <div className="movies_title">
+        <Typography variant="h4">Popular Movies</Typography>
+      </div>
     </div>
   );
 }
